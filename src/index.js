787912ead85ef3b4016be52ff4ed6a6fe585f081
iusmanof <iusmanof@gmail.com>
2020-10-08 22:02:35 +0300

    inti: start to solve

--- src/index.js
+++ src/index.js
@@ -38,8 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   
+  let everyTen = expr.match(/.{1,10}/g);
+  let r = everyTen.map((el) => {
+    let splitTwo = el.match(/.{1,2}/g);
+    
+
+    return convertNumberToSignal(splitTwo).join('');
+  })
+
+  
+  let word = convertSignalToLetter(r);
+  console.log(word);
 }
+  
+ function convertNumberToSignal(code){
+   return code.map((el) => {
+        if (el === "10"){
+          return ".";
+        }
+        else if(el === "11"){
+          return "-";
+        }
+        else{
+          return "";
+        }
+      });
+ }
+
+ function convertSignalToLetter(signal){
+   let word = '';
+   for (let s of signal){
+    
+    word += MORSE_TABLE[s];
+   }
+   return word;
+ }
+
 
 module.exports = {
     decode
